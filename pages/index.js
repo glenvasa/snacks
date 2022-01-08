b1c9cb9f68287b952f733e8d3f1e82cb2c246d36
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-08 14:07:49 -0500

    Displaying PizzaList comp below Featured comp on HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Featured from '../components/Featured'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>SNACKS! - Food Ordering App</title>
-        <meta name="description" content="Customize and order your favorite snacks!" />
+        <meta
+          name="description"
+          content="Customize and order your favorite snacks!"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <Featured/>
+      <Featured />
+      <PizzaList />
     </div>
-  )
+  );
 }
