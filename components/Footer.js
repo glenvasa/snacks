25dd478e7b0115ce1dddcd9f9022ac5ba91f40db
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-08 14:07:06 -0500

    added footer image and restaurant details

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,51 @@
+import Image from 'next/image'
+import styles from '../styles/Footer.module.css'
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <Image src="/images/bg.png" layout='fill' alt='' objectFit='cover'></Image>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.card}>
+                  <h2 className={styles.motto}>
+                      {`BEST PIZZA IN TOWN? 
+                      IT'S SNACKS. JUST FACTS!`}
+                  </h2>
+              </div>
+              <div className={styles.card}>
+                  <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+                  <p className={styles.text}>
+                      17542 Main St. 
+                      <br />Brockton, MA 02301
+                      <br />508-558-2681
+                  </p>
+                  <p className={styles.text}>
+                      754  N. Main St. 
+                      <br />Brockton, MA 02301
+                      <br />508-558-2682
+                  </p>
+                  <p className={styles.text}>
+                      542 S. Main St. 
+                      <br />Brockton, MA 02301
+                      <br />508-558-2683
+                  </p>
+                  <p className={styles.text}>
+                      175 E. Main St. 
+                      <br />Brockton, MA 02301
+                      <br />508-558-2684
+                  </p>
+              </div>
+              <div className={styles.card}>
+              <h1 className={styles.title}>HOURS</h1>
+              <p className={styles.text}>
+                      SUNDAY - SATURDAY
+                      <br />11:00AM - 10:00PM
+                      
+                  </p>
+              </div>
+            </div>
         </div>
     )
 }
