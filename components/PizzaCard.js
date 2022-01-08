4bb9216d95b3b8ac4f643904ef869ef2cf8e8a1d
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-08 14:16:08 -0500

    created Link to Product Page

--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from 'next/link'
 import styles from "../styles/PizzaCard.module.css";
 
 const PizzaCard = () => {
   return (
-    <div className={styles.container}>
+    <Link href='/product/1'>
+     
+ <div className={styles.container}>
       <Image src="/images/pizza.png" width={500} height={500} alt="pizza" />
       <h1 className={styles.title}>SPECIALE DI SNACKS</h1>
       <span className={styles.price}>$14.99</span>
@@ -11,6 +14,11 @@ const PizzaCard = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </p>
     </div>
+
+     
+   
+    </Link>
+    
   );
 };
 
