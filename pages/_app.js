39985d8d397ec2bbb00da8775d6023171f5e6792
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-09 12:33:21 -0500

    wrapped App in redux provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import store from '../redux/store'
+import {Provider} from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Provider store={store}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </Provider>
+    
     
   )
 }
