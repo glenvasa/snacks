6e8dcaaa795f1e8d874cf1df3a2db20a0da31d30
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-09 10:56:40 -0500

    displaying pizza info w/data received through props

--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -2,16 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/PizzaCard.module.css";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
-    <Link href="/product/1">
+    <Link href={`/product/${pizza._id}`} passHref>
       <div className={styles.container}>
-        <Image src="/images/pizza.png" width={500} height={500} alt="pizza" />
-        <h1 className={styles.title}>SPECIALE DI SNACKS</h1>
-        <span className={styles.price}>$14.99</span>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
+        <Image src={pizza.img} width={500} height={500} alt="pizza" />
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>${pizza.prices[0]}.00</span>
+        <p className={styles.desc}>{pizza.desc}</p>
       </div>
     </Link>
   );
