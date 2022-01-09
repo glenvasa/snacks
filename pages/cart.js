26ade09053d86130c7b255d58cb546801263c722
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-09 15:39:18 -0500

    added initial paypal functionality

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,103 @@
 import styles from "../styles/Cart.module.css";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { reset } from "../redux/cartSlice";
+// import OrderDetail from "../components/OrderDetail";
 
 const Cart = () => {
+
+  const [open, setOpen] = useState(false);
+  const [cash, setCash] = useState(false);
+ 
+  const currency = "USD";
+  const style = { layout: "vertical" };
+
+  const router = useRouter();
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
 
+  const amount = Number(cart.total * 1.0625).toFixed(2);
+  
   const tax = Number((cart.total * .0625).toFixed(2))
-  const grandTotal = cart.total + tax
+  const grandTotal = Number((cart.total + tax).toFixed(2))
+
+  const createOrder = async (data) => {
+    try {
+      const res = await axios.post("http://localhost:3000/api/orders", data);
+      if (res.status === 201) {
+        dispatch(reset());
+        router.push(`/orders/${res.data._id}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Custom component to wrap the PayPalButtons and handle currency changes
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+    // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+      dispatch({
+        type: "resetOptions",
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
+    }, [currency, showSpinner]);
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function (details) {
+              const shipping = details.purchase_units[0].shipping;
+              createOrder({
+                customer: shipping.name.full_name,
+                address: shipping.address.address_line_1,
+                total: cart.total,
+                method: 1,
+              });
+            });
+          }}
+        />
+      </>
+    );
+  };
 
   return (
     <div className={styles.container}>
@@ -74,9 +163,34 @@ const Cart = () => {
           <div className={styles.totalText}>
             <b className={styles.totalTextTitle}>Total:</b>${grandTotal}
           </div>
-          <button className={styles.button}>CHECKOUT NOW!</button>
+          {open ? (
+            <div className={styles.paymentMethods}>
+              <button
+                className={styles.payButton}
+                onClick={() => setCash(true)}
+              >
+                CASH ON DELIVERY
+              </button>
+              <PayPalScriptProvider
+                options={{
+                  "client-id":
+                    "ATTL8fDJKfGzXNH4VVuDy1qW4_Jm8S0sqmnUTeYtWpqxUJLnXIn90V8YIGDg-SNPaB70Hg4mko_fde4-",
+                  components: "buttons",
+                  currency: "USD",
+                  "disable-funding": "credit,card,p24",
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
+            </div>
+          ) : (
+            <button onClick={() => setOpen(true)} className={styles.button}>
+              CHECKOUT NOW!
+            </button>
+          )}
         </div>
       </div>
+      {/* {cash && <OrderDetail total={cart.total} createOrder={createOrder} />} */}
     </div>
   );
 };
