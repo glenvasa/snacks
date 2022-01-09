94a05a8c8cd7235f0d6cbee7ae748871d813e597
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-09 10:58:18 -0500

    now using data received through props from index.js

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -2,13 +2,13 @@ import styles from "../styles/PizzaList.module.css";
 import PizzaCard from "./PizzaCard";
 import Image from "next/image";
 
-const PizzaList = () => {
+const PizzaList = ({ pizzaList }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo} >
-         <Image src='/images/snacks.png' width={190} height={170} alt='logo' />
+      <div className={styles.logo}>
+        <Image src="/images/snacks.png" width={190} height={170} alt="logo" />
       </div>
-       
+
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
       <p className={styles.desc}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat libero
@@ -16,14 +16,7 @@ const PizzaList = () => {
         natus facere voluptatem eius quis consequuntur?
       </p>
       <div className={styles.wrapper}>
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
+        {pizzaList.map(pizza => <PizzaCard key={pizza._id} pizza={pizza}/>)};   
       </div>
     </div>
   );
