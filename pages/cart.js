4812954477415dbffa1fbcf60ab18e8f7dcc2be4
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-10 11:13:09 -0500

    Paypal configuration/implementation and createOrder functionality

--- pages/cart.js
+++ pages/cart.js
@@ -10,13 +10,12 @@ import {
 import axios from "axios";
 import { useRouter } from "next/router";
 import { reset } from "../redux/cartSlice";
-// import OrderDetail from "../components/OrderDetail";
+import OrderDetail from "../components/OrderDetail";
 
 const Cart = () => {
-
   const [open, setOpen] = useState(false);
   const [cash, setCash] = useState(false);
- 
+
   const currency = "USD";
   const style = { layout: "vertical" };
 
@@ -26,9 +25,9 @@ const Cart = () => {
   const cart = useSelector((state) => state.cart);
 
   const amount = Number(cart.total * 1.0625).toFixed(2);
-  
-  const tax = Number((cart.total * .0625).toFixed(2))
-  const grandTotal = Number((cart.total + tax).toFixed(2))
+
+  const tax = Number((cart.total * 0.0625).toFixed(2));
+  const grandTotal = Number((cart.total + tax).toFixed(2));
 
   const createOrder = async (data) => {
     try {
@@ -38,7 +37,7 @@ const Cart = () => {
         router.push(`/orders/${res.data._id}`);
       }
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
     }
   };
 
@@ -56,7 +55,7 @@ const Cart = () => {
           currency: currency,
         },
       });
-    }, [currency, showSpinner]);
+    }, [dispatch, options, currency, showSpinner]);
 
     return (
       <>
@@ -86,9 +85,14 @@ const Cart = () => {
           onApprove={function (data, actions) {
             return actions.order.capture().then(function (details) {
               const shipping = details.purchase_units[0].shipping;
+              console.log(shipping)
+              const addressInfo = Object.values(shipping.address)
+              console.log(addressInfo)
               createOrder({
                 customer: shipping.name.full_name,
-                address: shipping.address.address_line_1,
+                address: addressInfo,
+                  // shipping.address.address_line_1,
+                
                 total: cart.total,
                 method: 1,
               });
@@ -103,65 +107,68 @@ const Cart = () => {
     <div className={styles.container}>
       <div className={styles.left}>
         <table className={styles.table}>
-          <tr className={styles.trTitle}>
-            <th>Product</th>
-            <th>Name</th>
-            <th>Extras</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-          </tr>
-          {cart.products.map((product) => (
-            <tr className={styles.tr} key={product._id}>
-            <td>
-              <div className={styles.imgContainer}>
-                <Image
-                  src={product.img}
-                  layout="fill"
-                  objectFit="cover"
-                  alt=""
-                />
-              </div>
-            </td>
-            <td>
-              <span className={styles.name}>{product.title}</span>
-            </td>
-            <td className={styles.extrasContainer}>
-              {product.extrasArray.map(extra => (
-          <span key={extra._id} className={styles.extras}>
-               {extra.text}
-              </span>
-
-              ))}
-              
-            </td>
-            <td>
-              <span className={styles.price}>${product.totalPrice.toFixed(2)}</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>{product.quantity}</span>
-            </td>
-            <td>
-              <span className={styles.total}>${(product.totalPrice * product.quantity).toFixed(2)}</span>
-            </td>
-          </tr>
-
-          ))}
-          
-          
+          <tbody>
+            <tr className={styles.trTitle}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+          </tbody>
+          <tbody>
+            {cart.products.map((product) => (
+              <tr className={styles.tr} key={product._id}>
+                <td>
+                  <div className={styles.imgContainer}>
+                    <Image
+                      src={product.img}
+                      layout="fill"
+                      objectFit="cover"
+                      alt=""
+                    />
+                  </div>
+                </td>
+                <td>
+                  <span className={styles.name}>{product.title}</span>
+                </td>
+                <td className={styles.extrasContainer}>
+                  {product.extrasArray.map((extra) => (
+                    <span key={extra._id} className={styles.extras}>
+                      {extra.text}
+                    </span>
+                  ))}
+                </td>
+                <td>
+                  <span className={styles.price}>
+                    ${product.totalPrice.toFixed(2)}
+                  </span>
+                </td>
+                <td>
+                  <span className={styles.quantity}>{product.quantity}</span>
+                </td>
+                <td>
+                  <span className={styles.total}>
+                    ${(product.totalPrice * product.quantity).toFixed(2)}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>CART TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Subtotal:</b>${cart.total}
+            <b className={styles.totalTextTitle}>Subtotal:</b>${cart.total.toFixed(2)}
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Tax:</b>${tax}
+            <b className={styles.totalTextTitle}>Sales Tax:</b>${tax}
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>${grandTotal}
+            <b className={styles.totalTextTitle}>Order Total:</b>${grandTotal}
           </div>
           {open ? (
             <div className={styles.paymentMethods}>
@@ -174,7 +181,7 @@ const Cart = () => {
               <PayPalScriptProvider
                 options={{
                   "client-id":
-                    "ATTL8fDJKfGzXNH4VVuDy1qW4_Jm8S0sqmnUTeYtWpqxUJLnXIn90V8YIGDg-SNPaB70Hg4mko_fde4-",
+                    "AUH97Q9ISG2buY1AXhEOHDDf0cEfxQaSR7K3SCvmGVPlU9YY5PkqNxkkiY5YYrtmUgaiTw7LbUR2EN42",
                   components: "buttons",
                   currency: "USD",
                   "disable-funding": "credit,card,p24",
@@ -190,7 +197,7 @@ const Cart = () => {
           )}
         </div>
       </div>
-      {/* {cash && <OrderDetail total={cart.total} createOrder={createOrder} />} */}
+      {cash && <OrderDetail total={cart.total} createOrder={createOrder} />}
     </div>
   );
 };
