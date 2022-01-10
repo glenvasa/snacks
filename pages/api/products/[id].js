d34fcab2934826be8fad0af3fcfc1249aa2fece6
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-10 15:51:36 -0500

    added DELETE api req to delete a product and PUT api request to change a product

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -5,8 +5,10 @@ export default async function handler(req, res) {
   const {
     method,
     query: { id },
+    // cookies
   } = req;
 
+  // const token = cookies.token
   dbConnect();
 
   if (method === "GET") {
@@ -18,21 +20,29 @@ export default async function handler(req, res) {
     }
   }
 
-//   if (method === "PUT") {
-//     try {
-//       const product = await Product.create(req.body);
-//       res.status(201).json({ message: "Product created!", product });
-//     } catch (error) {
-//       res.status(500).json(error.message || "Something went wrong");
-//     }
-//   }
+  if (method === "PUT") {
+    // if(!token || token !== process.env.token){
+    //   return res.status(401).json("Not authenticated!")
+    // }
+    try {
+      const product = await Product.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 
-//   if (method === "DELETE") {
-//     try {
-//       const product = await Product.create(req.body);
-//       res.status(201).json({ message: "Product created!", product });
-//     } catch (error) {
-//       res.status(500).json(error.message || "Something went wrong");
-//     }
-//   }
+  if (method === "DELETE") {
+    // if(!token || token !== process.env.token){
+    //   return res.status(401).json("Not authenticated!")
+    // }
+    try {
+      await Product.findByIdAndDelete(id);
+      res.status(200).json("The product has been deleted!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 }
