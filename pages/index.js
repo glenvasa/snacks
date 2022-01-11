a596bb679fb215ec6033c97d8aedf8be922a48f2
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-11 10:27:25 -0500

    displaying Add button if admin prop is true (admin logged in and correct cookie stored)

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-// import {useEffect} from 'react'
+import {useState} from 'react'
 import axios from 'axios'
 import Head from "next/head";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
+import AddButton from '../components/AddButton';
 import styles from "../styles/Home.module.css";
+import AddProduct from '../components/AddProduct';
 
-export default function Home({pizzaList}) {
+export default function Home({pizzaList, admin}) {
+  const [close, setClose] = useState(true)
 
 // client side data fetching if not using Next
 //  const getProducts = async () => {
@@ -30,18 +33,29 @@ export default function Home({pizzaList}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose}/>}
       <PizzaList pizzaList={pizzaList}/>
+      {!close && <AddProduct setClose={setClose} />}
     </div>
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(ctx) {
+
+  const myCookie = ctx.req?.cookies || ''
+  let admin = false
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true
+  }
+
   const res = await axios.get('http://localhost:3000/api/products')
   
 
   return {
     props: {
-      pizzaList: res.data.products
+      pizzaList: res.data.products,
+      admin
     }
   }
 }
