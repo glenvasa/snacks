60c2116738b3a0b6c3d4d1e1343988329e5b5141
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-14 09:22:45 -0500

    Update index.js

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -4,7 +4,7 @@ import Order from "../../../models/Order";
 const handler = async (req, res) => {
     const { method } = req;
   
-    dbConnect();
+    await dbConnect();
   
     if (method === "GET") {
       try {
