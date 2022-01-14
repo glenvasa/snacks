5ff6d523ee1df32628b219d23935e62e92d0d179
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-14 08:56:34 -0500

    changed axios req url to vercel deployed domain instead of localhost

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -12,7 +12,7 @@ const Login = () => {
 
   const handleClick = async () => {
     try {
-      await axios.post("http://localhost:3000/api/login", {
+      await axios.post("http://snacks-psi.vercel.app/api/login", {
         username,
         password,
       });
