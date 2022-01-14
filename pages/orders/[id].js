61075f4a9a9b69788b93999f273800bfee388494
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-14 08:56:45 -0500

    changed axios req url to vercel deployed domain instead of localhost

--- pages/orders/[id].js
+++ pages/orders/[id].js
@@ -128,7 +128,7 @@ const Order = ({ order }) => {
 };
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+  const res = await axios.get(`http://snacks-psi.vercel.app/api/orders/${params.id}`);
   return {
     props: { order: res.data },
   };
