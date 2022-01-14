bc96851cac356d10f644b0694e2c2ba30fea2d6a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-14 09:02:24 -0500

    changed axios url to vercel deployed domain

--- pages/cart.js
+++ pages/cart.js
@@ -31,7 +31,7 @@ const Cart = () => {
 
   const createOrder = async (data) => {
     try {
-      const res = await axios.post("http://localhost:3000/api/orders", data);
+      const res = await axios.post("https://snacks-glenvasa.vercel.app/api/orders", data);
       if (res.status === 201) {
         dispatch(reset());
         router.push(`/orders/${res.data._id}`);
