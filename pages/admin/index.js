a9df2dacd8f24b9dba21ab83dd2713a0a40cb499
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-24 13:28:32 -0500

    moved products to admin/update page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,34 +1,21 @@
 import axios from "axios";
-import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import styles from "../../styles/Admin.module.css";
 
-const Admin = ({ orders, products }) => {
-  const [pizzaList, setPizzaList] = useState(products);
+const Admin = ({ orders }) => {
   const [orderList, setOrderList] = useState(orders);
   const status = ["preparing", "on the way", "delivered"];
 
-  const url = "http://localhost:3000" || "https://snacks-glenvasa.vercel.app"
-
-  const handleDelete = async (id) => {
-    console.log(id);
-    try {
-      const res = await axios.delete(
-        `${url}/api/products/` + id
-      );
-      setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
-    } catch (err) {
-      console.log(err.message || "Something went wrong");
-    }
-  };
+  const url = "http://localhost:3000" || "https://snacks-glenvasa.vercel.app";
 
   const handleStatus = async (id) => {
     const item = orderList.filter((order) => order._id === id)[0];
 
     const currentStatus = item.status;
 
-    if(currentStatus === 2){
-        return
+    if (currentStatus === 2) {
+      return;
     }
 
     try {
@@ -47,48 +34,15 @@ const Admin = ({ orders, products }) => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Products</h1>
-        <table className={styles.table}>
-          <tbody>
-            <tr className={styles.trTitle}>
-              <th>Image</th>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Action</th>
-            </tr>
-          </tbody>
-          {pizzaList.map((product) => (
-            <tbody key={product._id}>
-              <tr className={styles.trTitle}>
-                <td>
-                  <Image
-                    src={product.img}
-                    width={50}
-                    height={50}
-                    objectFit="cover"
-                    alt=""
-                  />
-                </td>
-                <td>{product._id.slice(0, 5)}...</td>
-                <td>{product.title}</td>
-                <td>${product.prices[0]}</td>
-                <td>
-                  <button className={styles.button}>Edit</button>
-                  <button
-                    className={styles.button}
-                    onClick={() => handleDelete(product._id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          ))}
-        </table>
-      </div>
-      <div className={styles.item}>
-        <h1 className={styles.title}>Orders</h1>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Admin Page: Orders</h1>
+          <Link href="/admin/update" passHref>
+            <span className={styles.updatePageLink}>
+              Products Page
+            </span>
+          </Link>
+        </div>
+
         <table className={styles.table}>
           <tbody>
             <tr className={styles.trTitle}>
@@ -103,7 +57,9 @@ const Admin = ({ orders, products }) => {
           {orderList.map((order) => (
             <tbody key={order._id}>
               <tr className={styles.trTitle}>
-                <td>{order._id.slice((order._id.length - 5), order._id.length)}</td>
+                <td>
+                  {order._id.slice(order._id.length - 5, order._id.length)}
+                </td>
                 <td>{order.customer}</td>
                 <td>${(order.total * 1.0625).toFixed(2)}</td>
                 <td>
@@ -125,7 +81,7 @@ const Admin = ({ orders, products }) => {
 };
 
 export const getServerSideProps = async (ctx) => {
-  const url = "http://localhost:3000" || "https://snacks-glenvasa.vercel.app"
+  const url = "http://localhost:3000" || "https://snacks-glenvasa.vercel.app";
   const myCookie = ctx.req?.cookies || "";
 
   if (myCookie.token !== process.env.TOKEN) {
