6ea5ec28e139b77e5d889d6da93c02a1d4ba7806
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-24 11:39:54 -0500

    links to Order Track page and Contact(footer)

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
+      <div className={styles.item_logo}>
         <div className={styles.callButton}>
           <Image src="/images/phone.png" width={32} height={32} alt='phone' />
         </div>
@@ -18,19 +18,27 @@ const Navbar = () => {
           <div className={styles.text}>508-558-2681</div>
         </div>
       </div>
-      <div className={styles.item}>
+      <div className={styles.item_list}>
           <ul className={styles.list}>
             <Link href='/' passHref>
               <li className={styles.listItem}>HomePage</li>
             </Link>
-          <Link href='#pizza-list' passHref>
-           <li className={styles.listItem}>Products</li>
+          <Link href='/#pizza-list' passHref>
+           <li className={styles.listItem}>Menu</li>
           </Link>
            
-            <li className={styles.listItem}>Menu</li>
-            <Image src='/images/snacks.png' width={190} height={170} alt='logo' className={styles.logo} />
-            <li className={styles.listItem}>Blog</li>
+            
+            <li>
+                  <Image src='/images/snacks.png' width={190} height={170} alt='logo' className={styles.logo} />
+            </li>
+        
+            <Link href='/track' passHref>
+            <li className={styles.listItem}>Order Track</li>
+            </Link>
+            <Link href='/#footer' passHref>
             <li className={styles.listItem}>Contact</li>
+            </Link>
+            {/* <li className={styles.listItem}>Blog</li> */}
             <Link href='/admin/login' passHref>
             <li className={styles.listItem}>Admin</li>
             </Link>
@@ -38,7 +46,7 @@ const Navbar = () => {
           </ul>
       </div>
       <Link href='/cart' passHref>
-      <div className={styles.item}>
+      <div className={styles.item_cart}>
           <div className={styles.cart}>
           <Image src='/images/cart.png' width={30} height={30} alt='logo' />
           <div className={styles.counter}>{quantity}</div>
