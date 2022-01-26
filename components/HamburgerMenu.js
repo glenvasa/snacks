99aa6612c80f4e1beb2436d8a5bab34e610c367f
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-26 10:52:11 -0500

    Create HamburgerMenu.js

---
+++ components/HamburgerMenu.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+
+const HamburgerMenu = () => {
+  return (
+    <div className={styles.hamMenuItem_list}>
+      <ul className={styles.hamMenu}>
+        {/* <Link href="/" passHref>
+          <li className={styles.hamMenuItem}>HomePage</li>
+        </Link> */}
+        <Link href="/#pizza-list" passHref>
+          <li className={styles.hamMenuItem}>Menu</li>
+        </Link>
+
+        {/* <li>
+                  <Image src='/images/snacks.png' width={190} height={170} alt='logo' className={styles.logo} />
+            </li> */}
+<Link href="/cart" passHref>
+          <li className={styles.hamMenuItem}>Cart</li>
+        </Link>
+        <Link href="/track" passHref>
+          <li className={styles.hamMenuItem}>Order Track</li>
+        </Link>
+        <Link href={"/#motto"} passHref>
+          <li className={styles.hamMenuItem}>Contact</li>
+        </Link>
+        {/* <li className={styles.hamMenuItem}>Blog</li> */}
+        <Link href="/admin/login" passHref>
+          <li className={styles.hamMenuItem}>Admin</li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
