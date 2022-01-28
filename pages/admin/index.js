6f300b4762ce25e8bf9f4bb45002bdfe0c51aadf
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-28 12:12:36 -0500

    added a delete order button w/functionality

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 import Link from "next/link";
-import { useState } from "react";
-import {useRouter} from 'next/router'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import styles from "../../styles/Admin.module.css";
 
 const Admin = ({ orders }) => {
   const [orderList, setOrderList] = useState(orders);
   const status = ["preparing", "on the way", "delivered"];
 
-  const url = process.env.URL
+  const url = process.env.URL;
 
-  const router = useRouter()
+  const router = useRouter();
 
-  if (typeof window !== 'undefined') {
-localStorage.getItem('admin') === 'true' ? null : router.push('/admin/login')
+  if (typeof window !== "undefined") {
+    localStorage.getItem("admin") === "true"
+      ? null
+      : router.push("/admin/login");
   }
-  
-  
+
   const handleStatus = async (id) => {
     const item = orderList.filter((order) => order._id === id)[0];
 
@@ -39,10 +40,26 @@ localStorage.getItem('admin') === 'true' ? null : router.push('/admin/login')
     }
   };
 
-  const logout = () => {
-    localStorage.clear()
-    router.push('/')
+  const handleDeleteOrder = async (id) => {
+   confirm("Are you sure you want to delete this order?")
+      try {
+        await axios.delete(`/api/orders/` + id)
+      } catch (err) {
+        console.log(err.message || "Something went wrong")
+      }
+    alert("Order deleted")
   }
+
+
+  const logout = () => {
+    localStorage.clear();
+    router.push("/");
+  };
+
+
+  // useEffect(()=> {
+
+  // }, [orderList])
 
   return (
     <div className={styles.container}>
@@ -50,19 +67,18 @@ localStorage.getItem('admin') === 'true' ? null : router.push('/admin/login')
         <div className={styles.titleContainer}>
           <h1 className={styles.title}>Admin Page: Orders</h1>
           <Link href="/admin/update" passHref>
-            <span className={styles.updatePageLink}>
-              Products Page
-            </span>
-            
+            <span className={styles.updatePageLink}>Products Page</span>
           </Link>
-          <button className={styles.logout} onClick={() => logout()}>Admin Logout</button>
+          <button className={styles.logout} onClick={() => logout()}>
+            Admin Logout
+          </button>
         </div>
 
         <table className={styles.table}>
           <tbody>
             <tr className={styles.trTitle}>
               <th>Customer</th>
-              <th>Address</th> 
+              <th>Address</th>
               <th>Order</th>
               <th>Total</th>
               <th>Payment</th>
@@ -72,27 +88,29 @@ localStorage.getItem('admin') === 'true' ? null : router.push('/admin/login')
           </tbody>
           {orderList.map((order) => (
             <>
-            <tbody key={order._id}>
-              <tr className={styles.trTitle}>
-                {/* <td>
+              <tbody key={order._id}>
+                <tr className={styles.trTitle}>
+                  {/* <td>
                   {order._id.slice(order._id.length - 5, order._id.length)}
                 </td> */}
-                <td>{order.customer}</td>
-                <td>
-                  <div className={styles.address}>
-                    <span className={styles.address}>{order.address[0]} </span>
-                    <span
-                      className={styles.address}
-                    >{`${order.address[1]}, ${order.address[2]}, ${order.address[3]}`}</span>
-                  </div>
-                
-                
-                </td>
-                
+                  <td>{order.customer}</td>
+                  <td>
+                    <div className={styles.address}>
+                      <span className={styles.address}>
+                        {order.address[0]}{" "}
+                      </span>
+                      <span
+                        className={styles.address}
+                      >{`${order.address[1]}, ${order.address[2]}, ${order.address[3]}`}</span>
+                    </div>
+                  </td>
+
                   <td className={styles.products}>
                     {order.products.map((product) => (
                       <div key={product.index}>
-                        <span>{`${product.quantity} ${product.title}${product.quantity > 1 ? `s` : ''} with `}</span>
+                        <span>{`${product.quantity} ${product.title}${
+                          product.quantity > 1 ? `s` : ""
+                        } with `}</span>
 
                         {product.extrasArray.map((extra, index) => (
                           <span key={extra.index}>
@@ -107,20 +125,31 @@ localStorage.getItem('admin') === 'true' ? null : router.push('/admin/login')
                       </div>
                     ))}
                   </td>
-                
-                <td>${(order.total * 1.0625).toFixed(2)}</td>
-                <td>
-                  {order.method === 0 ? <span>cash</span> : <span>paid</span>}
-                </td>
-                <td>{status[order.status]}</td>
-                <td>
-                  <button className={styles.button_stage} onClick={() => handleStatus(order._id)}>
-                    Next Stage
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-            <div className={styles.spacer}/>
+
+                  <td>${(order.total * 1.0625).toFixed(2)}</td>
+                  <td>
+                    {order.method === 0 ? <span>CASH</span> : <span>PAID</span>}
+                  </td>
+                  <td>{status[order.status].toUpperCase()}</td>
+                  <td>
+                    <span className={styles.actionButtons}>
+                      <button
+                        className={styles.button_stage}
+                        onClick={() => handleStatus(order._id)}
+                      >
+                        Next Stage
+                      </button>
+                      <button
+                        className={styles.button_deleteOrder}
+                        onClick={() => handleDeleteOrder(order._id)}
+                      >
+                        Delete
+                      </button>
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+              <div className={styles.spacer} />
             </>
           ))}
         </table>
@@ -130,7 +159,7 @@ localStorage.getItem('admin') === 'true' ? null : router.push('/admin/login')
 };
 
 export const getServerSideProps = async (ctx) => {
-  const url = process.env.URL
+  const url = process.env.URL;
   // const myCookie = ctx.req?.cookies || "";
 
   // if (myCookie.token !== process.env.TOKEN) {
