aa4439c55ed7c296a55f0b94dd2afc47a5e6126a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-29 17:20:51 -0500

    commented out cookie code since using localStorage

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,15 +1,15 @@
-import cookie from 'cookie'
+// import cookie from 'cookie'
 
 const handler = async (req, res) => {
     if (req.method === 'POST') {
         const {username, password } = req.body
         if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
-            res.setHeader('Set-Cookie', cookie.serialize('token', process.env.TOKEN, {
-                maxAge: 60 * 60,
-                sameSite: 'strict',
-                path: '/',
-            })
-            )
+            // res.setHeader('Set-Cookie', cookie.serialize('token', process.env.TOKEN, {
+            //     maxAge: 60 * 60,
+            //     sameSite: 'strict',
+            //     path: '/',
+            // })
+            // )
             res.status(200).json('Successful admin login')
         } else {
             res.status(400).json('Wrong admin credentails entered')
