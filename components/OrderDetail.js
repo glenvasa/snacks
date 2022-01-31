bcb738355dbe0e9be56dcb4e0c0f65c1f29b5007
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-31 11:41:38 -0500

    styling so that modal doesn't take up entire screen, better display, etc.

--- components/OrderDetail.js
+++ components/OrderDetail.js
@@ -38,12 +38,12 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           X
         </button>
         <h1 className={styles.title}>
-          You will pay ${totalTaxIncluded} on delivery.
+          You will pay <span className={styles.total}>${totalTaxIncluded}</span> on delivery
         </h1>
         <div className={styles.item}>
           <label className={styles.label}>Name</label>
           <input
-            placeholder="John Doe"
+            // placeholder="John Doe"
             type="text"
             className={styles.input}
             onChange={(e) => setCustomer(e.target.value)}
@@ -53,7 +53,7 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           <label className={styles.label}>Phone Number</label>
           <input
             type="text"
-            placeholder="555-555-1212"
+            // placeholder="617-555-1212"
             className={styles.input}
             onChange={(e) => setPhone(e.target.value)}
           />
@@ -62,7 +62,7 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           <label className={styles.label}>Street Address</label>
           <input
             type="text"
-            placeholder="12345 Main St."
+            // placeholder="12345 Main St., Apt. 212"
             className={styles.input}
             onChange={(e) => setStreet(e.target.value)}
           />
@@ -71,7 +71,7 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           <label className={styles.label}>City</label>
           <input
             type="text"
-            placeholder="Brockton"
+            // placeholder="Boston"
             className={styles.input}
             onChange={(e) => setCity(e.target.value)}
           />
@@ -80,7 +80,7 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           <label className={styles.label}>State</label>
           <input
             type="text"
-            placeholder="MA"
+            // placeholder="MA"
             className={styles.input}
             onChange={(e) => setState(e.target.value)}
           />
@@ -89,7 +89,7 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           <label className={styles.label}>Zip Code</label>
           <input
             type="text"
-            placeholder="02301"
+            // placeholder="02121"
             className={styles.input}
             onChange={(e) => setZip(e.target.value)}
           />
@@ -98,7 +98,7 @@ const OrderDetail = ({ total, createOrder, setCashOpen }) => {
           <label className={styles.label}>Email Address</label>
           <input
             type="email"
-            placeholder="customer@test.com"
+            // placeholder="john.doe@test.com"
             className={styles.input}
             onChange={(e) => setEmail(e.target.value)}
           />
