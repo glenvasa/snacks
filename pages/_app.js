8ef75515bf50b3b3ce6aeccff47e700f5921a591
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-02-02 09:07:38 -0500

    using NProgress to show loading when route is loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
+import NProgress from "nprogress";
+import Router from "next/router";
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import store from '../redux/store'
 import {Provider} from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({showSpinner: true})
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
+
+
   return (
     <Provider store={store}>
     <Layout>
